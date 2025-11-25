534678ee3d548c0ee068f0b82de0e1aa0109c61c
Spoekle <thiangraber@gmail.com>
2025-11-25 13:39:26 +0100

    rename overzicht to dashboard

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import SideBar from './components/SideBar'
 
 import Home from './pages/Home'
-import Overzicht from './pages/Overzicht/Index';
+import Dashboard from './pages/Dashboard/Index';
 import Druppels from './pages/Druppels/Index';
 import Personen from './pages/Personen/Index';
 
@@ -11,15 +11,15 @@ import "./App.css";
 function App() {
   return (
     <Router>
-        <div className='flex'>
-          <SideBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/overzicht" element={<Overzicht />} />
-            <Route path="/druppels" element={<Druppels />} />
-            <Route path="/personen" element={<Personen />} />
-          </Routes>
-        </div>
+      <div className='flex'>
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/druppels" element={<Druppels />} />
+          <Route path="/personen" element={<Personen />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
