97d50f2886b0b0da57d1172d0e3acaefffc2d552
ScolipedeFucker <maynouari@gmail.com>
2025-11-27 09:31:20 +0100

    Practice file c++

---
+++ AI/practice.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+//string p;
+
+void testFunc(string p = "hello", string o = "stringo") {
+    cout << p;
+    cout << o;
+}
+
+string combineStrings(string in1, string in2) {
+    string output = in1 + in2;
+
+    return output;
+}
+
+
+int main(){
+
+    double myNum1 = 5.0;
+    double myNum2 = 2.0;
+    string input1;
+    string input2;
+
+    string myArray[3] = {"arrOne", "arrTwo", "arrThree"};
+    vector<string> colors;
+
+    cin >> input1;
+    cin >> input2;
+//    int hello = 1;
+//    string hi = "hello";
+//    cin >> hi;
+//    cout << hi;
+
+//    double twoandahalf = myNum1 / myNum2;
+    cout << myNum1 + myNum2 << "\n";
+//    cout << twoandahalf << "\n";
+    cout << myNum1 / myNum2 << "\n";
+    cout << myNum1 * myNum2 << "\n";
+//    cout << myNum1 % myNum2 << "\n";
+
+//    if (hi !="undef") {
+//        testFunc("poop");
+//    } else {
+//        testFunc("piss", hi);
+//    }
+    for (int i = 0; i < 5; i++){
+//        cin >>
+        cout << i << "\n";
+    }
+//    cout << colors[2] << "\n";
+    cout << myArray[0] << "\n";
+    cout << combineStrings(input1, input2);
+}
