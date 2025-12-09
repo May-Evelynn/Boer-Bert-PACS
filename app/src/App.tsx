77bac4d9937df90dc39e2dd063a77ab6522676dd
Spoekle <thiangraber@gmail.com>
2025-12-09 09:38:12 +0100

    add gebruikers page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,27 +5,32 @@ import SideBar from './components/SideBar'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard/Index';
 import Druppels from './pages/Druppels/Index';
-import Personen from './pages/Personen/Index';
+import Gasten from './pages/Gasten/Index';
+import Gebruikers from './pages/Gebruikers/Index';
 
 import "./App.css";
+import "./CustomScrollbar.css"
 
 function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
+
   return (
 
     <Router>
-      <div className='select-none flex'>
+      <div className='select-none flex overflow-hidden h-screen w-screen'>
         <SideBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-        <Routes>
-          <Route path="/" element={<Home isLoggedIn={isLoggedIn} />} />
-          <Route path="/dashboard" element={<Dashboard isLoggedIn={isLoggedIn}/>} />
-          <Route path="/druppels" element={<Druppels isLoggedIn={isLoggedIn} />} />
-          <Route path="/personen" element={<Personen isLoggedIn={isLoggedIn} />} />
-        </Routes>
+        <div className='flex w-full h-full overflow-auto custom-scrollbar'>
+          <Routes>
+            <Route path="/" element={<Home isLoggedIn={isLoggedIn} />} />
+            <Route path="/dashboard" element={<Dashboard isLoggedIn={isLoggedIn} />} />
+            <Route path="/druppels" element={<Druppels isLoggedIn={isLoggedIn} />} />
+            <Route path="/gasten" element={<Gasten isLoggedIn={isLoggedIn} />} />
+            <Route path='/gebruikers' element={<Gebruikers isLoggedIn={isLoggedIn} />} />
+          </Routes>
+        </div>
       </div>
-    </Router>
+    </Router >
   );
 }
 
